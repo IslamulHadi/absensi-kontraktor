1f8ae6fc1e6fc348e143240c9a560cb906a2792e
seijuro <seijushiro@gmail.com>
2026-04-22 14:25:07 +0800

    wip

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,6 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login(Login::class)
+            ->favicon(asset('favicon.svg'))
             ->colors([
                 'primary' => Color::Sky,
             ])
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/favicon.svg', function () {
+    $path = public_path('favicon.svg');
+
+    if (! is_file($path)) {
+        abort(404);
+    }
+
+    return response()->file($path, [
+        'Content-Type' => 'image/svg+xml; charset=utf-8',
+    ]);
+})->name('favicon.svg');
+
 Route::get('/apk/absen.apk', function () {
     $path = public_path('apk/absen.apk');
 
